cd4038970647c38c84947784f938284ac4b55aed
taylor <tsburdette@gmail.com>
2017-05-27 17:12:13 -0400

    Adds documentation and catches missing json file.
    We can now allow json file to be missing in action and new one to be created.
    Still need to do the same for log files. Currently, touched blank logs.
    The primary reason for not accounting for missing files is that we would
    like to restructure to allow aggregation by timestamp.
    
    Cron job is being run by the current user, not root, now. This is
    advantageous because we can put the expectation of permissions on
    operations and not developers, who can be prone to making mistakes.
    
    We are also okay with the cron job creating a new isntance of
    Diagnostics because we are attempting to load from the file before
    trying to create a new json dictionary.

--- client.py
+++ client.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from crontab import CronTab
+import getpass
 import json
 import diagnostics
 
-cron = CronTab(user='root')
+""" Run cron to record new diagnostics periodically.
+TODO: Add arguments for user and time interval.
+"""
+cron = CronTab(user=getpass.getuser())
 job = cron.new(command='python ./diagnostics.py')
 job.minute.every(1)
 
-cron.write
+cron.write()
 
 #diags = diagnostics.Diagnostics()
 #diags.record_all_metrics()
@@ -18,8 +22,11 @@ api = Api(app)
 
 class CurrentMetrics(Resource):
     def get(self):
-        with open('diags.json', 'r') as diags_json:
-            return json.load(diags_json)
+        try:
+            with open('diags.json', 'r') as diags_json:
+                return json.load(diags_json)
+        except IOError:
+            return json.dumps({'Error': 'No data recorded yet.'})
 
 api.add_resource(CurrentMetrics, '/CurrentMetrics')
 
